7aea0d17adae681a066631b738d32b6a922b8e6d
Yukihiro Okada <yuokada@users.noreply.github.com>
2025-04-21 03:02:13 +0900

    Fix

--- src/main/java/com/baeldung/antlr/hello/HelloMain.java
+++ src/main/java/com/baeldung/antlr/hello/HelloMain.java
@@ -10,6 +10,7 @@ import org.antlr.v4.runtime.tree.*;
 public class HelloMain {
     public static void main(String[] args) throws Exception {
         // Create a CharStream that reads from standard input
+        System.out.printf("Enter a greeting message (Press ENTER and then Ctrl+D): ");
         CharStream input = CharStreams.fromStream(System.in);
 
         // Create a lexer that feeds off of the input CharStream
@@ -23,8 +24,7 @@ public class HelloMain {
 
         // Begin parsing at the 'greeting' rule
         ParseTree tree = parser.greeting();
-
         // Print the LISP-style tree
-        System.out.println(tree.toStringTree(parser));
+        System.out.println("Parse tree: " + tree.toStringTree(parser));
     }
 }
